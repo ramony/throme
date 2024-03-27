98baa76fbfa0d8ade91c179205edde927c1867a3
jianwei1 <jianwei1@michaels.com>
2024-03-27 17:41:51 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,24 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from "path";
 
+import electron from 'vite-electron-plugin'
+import renderer from 'vite-plugin-electron-renderer'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    electron({
+      include: ['electron'],
+      main: {
+        entry: 'electron/index.js', // 此处指向electron主进程文件
+      },
+    }),
+    // 渲染进程可使用node.js API
+    renderer({
+      nodeIntegration: true,
+    })
+  ],
   resolve: {
     alias: [{
       find: "@",
